ca9da023ecb5be7d01ceec009111030873e13722
tomjuggler <tomjuggler@gmail.com>
2023-11-27 16:31:59 +0200

    added more test html - "again" one works sort of added curl examples for some additional functionality
    Signed-off-by: tomjuggler <tomjuggler@gmail.com>

---
+++ curl-tests.sh
@@ -0,0 +1,27 @@
+# Note: can also work by pasting into a browser url bar (without "curl") and pressing enter
+
+# switch on router mode: 
+curl http://192.168.1.1/router?router=0
+curl http://192.168.1.78/router?router=0
+
+# switch off router mode: 
+curl http://192.168.1.1/router?router=1
+curl http://192.168.1.78/router?router=1
+
+# change pattern: 
+# pattern options: 
+# 1. on-device computer generated patterns
+# 2. images 1-5
+# 3. images 1-10
+# 4. images 11-20
+# 5. images 1-50 (52?)
+curl http://192.168.1.1/pattern?patternChooserChange=1
+curl http://192.168.1.78/pattern?patternChooserChange=1
+
+# change speed (integer): 
+# the time between image changes
+# interval is set to 2 seconds by default
+# minimum is 0 - converts to half second interval
+# maximum is 1800 (half an hour) - any more and re-set to 30min 
+curl http://192.168.1.1/intervalChange?interval=2
+curl http://192.168.1.78/intervalChange?interval=2
